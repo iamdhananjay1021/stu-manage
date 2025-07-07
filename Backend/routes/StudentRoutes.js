b9d5f769be2189220904370af052e79717b1979f
iamdhananjay1021 <pandeydhananjay1444@gmail.com>
2025-07-07 21:48:51 +0530

    Update StudentRoutes.js

--- Backend/routes/StudentRoutes.js
+++ Backend/routes/StudentRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const {studentRegistration, searchStudent, studentList, deleteStudent,studentDetails } = require("../controllers/studentControllers");
-const validateStudent = require("../middleware/Auth");
+const validateStudent = require("../middleware/auth");
 const router = express.Router();
 
 router.post("/registration", validateStudent, studentRegistration);
